81c2620fe7f038e192f02ded2b5018f852827900
oksanabolbat <oksana.bolbat@gmail.com>
2024-03-08 01:52:58 +0100

    not found and loading pages

---
+++ app/loading.jsx
@@ -0,0 +1,7 @@
+export default async function LoadingPage() {
+    return (
+        <div className="py-24 text-center font-semibold">
+            <p>loading</p>
+        </div>
+    );
+}
---
+++ app/not-found.jsx
@@ -0,0 +1,7 @@
+export default async function NotFoundPage() {
+    return (
+        <div className="py-24 text-center font-semibold">
+            <p>Please try other route!</p>
+        </div>
+    );
+}
